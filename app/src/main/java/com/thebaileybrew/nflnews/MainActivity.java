557e98bb6842de4cf4e60d56581583b4b4c747ef
Matthew Bailey <matthew.bailey@hthackney.com>
2018-06-30 15:31:12 -0400

    Updated with ButterKnife Binding

--- app/src/main/java/com/thebaileybrew/nflnews/MainActivity.java
+++ app/src/main/java/com/thebaileybrew/nflnews/MainActivity.java
@@ -20,73 +20,57 @@ import android.widget.RelativeLayout;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Football>> {
     private static final String LOG_TAG = MainActivity.class.getName();
-
     private static final String REQUEST_URL_ONE =
-            "https://content.guardianapis.com/search?from-date=2018-06-01&to-date=2018-07-01&q=NFL&api-key=9722bfef-08bf-4706-b3f0-a914a1dc5339";
-
+            "https://content.guardianapis.com/search?from-date=2018-05-01&to-date=2018-07-01&q=NFL&api-key=9722bfef-08bf-4706-b3f0-a914a1dc5339";
     private FootballAdapter footballAdapter;
     private static final int FOOTBALL_LOADER_ID = 1;
-
-    ListView footballListView;
-    RelativeLayout noActiveNetwork;
-    RelativeLayout loadingScreen;
+    //ButterKnife Views to Bind
+    @BindView(R.id.nfl_news_list) ListView footballListView;
+    @BindView(R.id.no_network_layout) RelativeLayout noActiveNetwork;
+    @BindView(R.id.loading_layout) RelativeLayout loadingScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        footballListView = findViewById(R.id.nfl_news_list);
-        noActiveNetwork = findViewById(R.id.no_network_layout);
-        loadingScreen = findViewById(R.id.loading_layout);
-
+        ButterKnife.bind(this);
         footballAdapter = new FootballAdapter(this,new ArrayList<Football>());
+        //Assign FootballAdapter and set onClick
         footballListView.setAdapter(footballAdapter);
-
         footballListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Football currentFootballNews = footballAdapter.getItem(position);
-
                 Uri footballUri = Uri.parse(currentFootballNews.getArticleUrl());
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, footballUri);
                 startActivity(websiteIntent);
             }
         });
-
         if (isNetworkAvailable()) {
             LoaderManager loaderManager = getLoaderManager();
-
             loaderManager.initLoader(FOOTBALL_LOADER_ID, null, this);
         } else {
             loadingScreen.setVisibility(GONE);
-
             footballListView.setEmptyView(noActiveNetwork);
         }
-
-
-
-
-
     }
-
     @Override
     public Loader<List<Football>> onCreateLoader(int id, Bundle args) {
         return new FootballLoader(this, REQUEST_URL_ONE);
     }
-
     @Override
     public void onLoadFinished(Loader<List<Football>> loader, List<Football> FootballNews) {
         if (!isNetworkAvailable()) {
             footballListView.setEmptyView(noActiveNetwork);
-            //set NO Network Display
         }
         footballAdapter.clear();
-
         //Check for valid dataset
         //add to adapter data set. This will trigger ListView to update
         if (isNetworkAvailable()) {
@@ -99,9 +83,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
         noActiveNetwork.setVisibility(View.INVISIBLE);
         footballAdapter.addAll(FootballNews);
-
-
-
     }
 
     @Override
@@ -111,10 +92,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     public boolean isNetworkAvailable(){
         ConnectivityManager connectionCheck = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo activeNetwork = connectionCheck.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
-
         if (isConnected) {
             return true;
         } else {
--- app/src/main/java/com/thebaileybrew/nflnews/QueryUtils.java
+++ app/src/main/java/com/thebaileybrew/nflnews/QueryUtils.java
@@ -19,26 +19,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QueryUtils {
-
     //Tag for LOG message
     private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
-    private QueryUtils() {
-
-    }
-
+    private QueryUtils() { }
     //Query the Guardian dataset and return the list of (@link Football) objects
     public static List<Football> fetchNewsData(String requestedUrl) {
-
         try {
             Thread.sleep(1000);
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
-
         //Create the URL object
         URL newsUrl = createUrl(requestedUrl);
-
         //Perform the HTTP Request
         String jsonResponse = null;
         try {
@@ -46,13 +39,11 @@ public class QueryUtils {
         } catch (IOException ioe) {
             Log.e(LOG_TAG, "fetchNewsData: Problem making HTTP Request.", ioe);
         }
-
         //Extract the relevant fiels from the JSON response and create a list of Football news
         List<Football> FootballNews = extractNewsFromJson(jsonResponse);
         Log.v("Fetch Method", "Yes");
         return FootballNews;
     }
-
     //Returns a hew URL object from the given String URL
     private static URL createUrl(String requestedUrl) {
         URL url = null;
@@ -63,11 +54,9 @@ public class QueryUtils {
         }
         return url;
     }
-
     //Make an HTTP Request and return a string as response
     private static String makeHttpRequest(URL url) throws IOException {
         String jsonResponse = "";
-
         //Checks the URL for null value
         if(url == null) {
             return jsonResponse;
@@ -100,7 +89,6 @@ public class QueryUtils {
         }
         return jsonResponse;
     }
-
     //Convert the InputStream data into a String which contains the entire JSON response from API
     private static String readFromStream(InputStream inputStream) throws IOException {
         StringBuilder output = new StringBuilder();
@@ -115,7 +103,7 @@ public class QueryUtils {
         }
         return output.toString();
     }
-
+    //Return the List<> extracted from the JSON data
     private static List<Football> extractNewsFromJson(String newsJSON) {
         String title;
         String date;
@@ -126,31 +114,24 @@ public class QueryUtils {
         if (TextUtils.isEmpty(newsJSON)) {
             return null;
         }
-
         List<Football> FootballNews = new ArrayList<>();
-
         try {
             JSONObject baseJsonResponse = new JSONObject(newsJSON);
             Log.v("Json Array", "NewsArray");
             JSONObject baseJsonResponseResult = baseJsonResponse.getJSONObject("response");
             JSONArray currentNewsArray = baseJsonResponseResult.getJSONArray("results");
-
             // For each news in the newsArray, create an {@link News} object
             for (int i = 0; i < currentNewsArray.length(); i++) {
                 JSONObject currentNews = currentNewsArray.getJSONObject(i);
-
                 // Extract the value for the key called "webTitle" -- Article Title
                 title = currentNews.getString("webTitle");
                 Log.v("title", title);
-
                 // Extract the value for the key called "pillarName" -- Article Category / Drawable
                 source = currentNews.getString("pillarName");
                 Log.v("category", source);
-
                 // Extract the value for the key called "webUrl" -- Article URL Address for onClick
                 url = currentNews.getString("webUrl");
                 Log.v("url", url);
-
                 // Extract the value for the key called "webPublicationDate" -- Article Date/Time
                 String rawdate = currentNews.getString("webPublicationDate");
                 Log.v("rawdate", rawdate);
